6be31da1e0e5156f8b3fced3b2a16c7c267b9187
Asrorkhodja Tursunkhodjayev <asrorkhudja@172-15-113-232.lightspeed.lnngmi.sbcglobal.net>
2026-04-16 12:49:20 +0500

    corrected coordinates

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -2,10 +2,12 @@ import photo_1 from "./photo_1.png"
 import photo_2 from "./photo_2.png"
 import anarfresh_3 from "./anarfresh_3.png"
 import logo from "./logo.png"
+import marker from "./marker.png"
 
 export const assets = {
     photo_1,
     photo_2,
     anarfresh_3,
-    logo
+    logo,
+    marker
 }
