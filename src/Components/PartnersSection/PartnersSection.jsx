e3d3f8422e05fc0e57682491ac4a8cd1cb85aaa0
Asrorkhodja Tursunkhodjayev <asrorkhudja@Asrorkhodjas-MacBook-Air.local>
2026-04-23 21:37:16 +0500

    partners

--- src/Components/PartnersSection/PartnersSection.jsx
+++ src/Components/PartnersSection/PartnersSection.jsx
@@ -1,30 +1,38 @@
 import React from "react";
 import "./PartnersSection.css";
-import { assets } from "../../assets/assets";
+import Shakhriyor from "../../assets/Partners/Shakhriyor.PNG";
+import Abdurahim from "../../assets/Partners/abdurahim_grid_aziz_tolaganov.PNG";
+import Aleksandr from "../../assets/Partners/aleksandr_orlov.PNG";
+import Bekhzodbeck from "../../assets/Partners/bekhzodbeck.PNG";
+import BonuIkromova from "../../assets/Partners/bonu_ikromova.PNG";
+import Choynakningqopqogi from "../../assets/Partners/choynakningqopqogi.PNG";
+import DavronVahobov from "../../assets/Partners/davron_vahobov.PNG";
+import GulomnNasriddinov from "../../assets/Partners/gulom_nasriddinov.PNG";
+import Ibrohim from "../../assets/Partners/ibrohim.PNG";
+import MassaShukhrat from "../../assets/Partners/massa_shukhrat_allayarov.PNG";
+import RashidKhalikov from "../../assets/Partners/rashid_khalikov.PNG";
+import SaadullaAbdullayev from "../../assets/Partners/saadulla_abdullayev_texnoplov.PNG";
+import Ulugbek from "../../assets/Partners/ulugbek.PNG";
+import UlugbekQozogiston from "../../assets/Partners/ulugbek_qozogiston.PNG";
+import UsomVosiyev from "../../assets/Partners/usoma_vosiyev.PNG";
 
 const PartnersSection = ({ t }) => {
   const partners = [
-    {
-      id: 1,
-      name: "Иван Петров",
-      title: "Основатель бренда",
-      image: assets.photo_1,
-      instagram: "https://instagram.com"
-    },
-    {
-      id: 2,
-      name: "Алиса Смирнова",
-      title: "Генеральный директор",
-      image: assets.photo_2,
-      instagram: "https://instagram.com"
-    },
-    {
-      id: 3,
-      name: "Владимир Козлов",
-      title: "Главный эксперт",
-      image: assets.anarfresh_3,
-      instagram: "https://instagram.com"
-    }
+    { id: 1, name: "Shakhriyor", title: "Shakhriyor", image: Shakhriyor, instagram: "https://instagram.com" },
+    { id: 2, name: "ulugbek_qozogiston", title: "ulugbek_qozogiston", image: UlugbekQozogiston, instagram: "https://instagram.com" },
+    { id: 3, name: "rashid_khalikov", title: "rashid_khalikov", image: RashidKhalikov, instagram: "https://instagram.com" },
+    { id: 4, name: "saadulla_abdullayev_texnoplov", title: "saadulla_abdullayev_texnoplov", image: SaadullaAbdullayev, instagram: "https://instagram.com" },
+    { id: 5, name: "ulugbek", title: "ulugbek", image: Ulugbek, instagram: "https://instagram.com" },
+    { id: 6, name: "bonu_ikromova", title: "bonu_ikromova", image: BonuIkromova, instagram: "https://instagram.com" },
+    { id: 7, name: "abdurahim_grid_aziz_tolaganov", title: "abdurahim_grid_aziz_tolaganov", image: Abdurahim, instagram: "https://instagram.com" },
+    { id: 8, name: "gulom_nasriddinov", title: "gulom_nasriddinov", image: GulomnNasriddinov, instagram: "https://instagram.com" },
+    { id: 9, name: "aleksandr_orlov", title: "aleksandr_orlov", image: Aleksandr, instagram: "https://instagram.com" },
+    { id: 10, name: "bekhzodbeck", title: "bekhzodbeck", image: Bekhzodbeck, instagram: "https://instagram.com" },
+    { id: 11, name: "choynakningqopqogi", title: "choynakningqopqogi", image: Choynakningqopqogi, instagram: "https://instagram.com" },
+    { id: 12, name: "davron_vahobov", title: "davron_vahobov", image: DavronVahobov, instagram: "https://instagram.com" },
+    { id: 13, name: "ibrohim", title: "ibrohim", image: Ibrohim, instagram: "https://instagram.com" },
+    { id: 14, name: "massa_shukhrat_allayarov", title: "massa_shukhrat_allayarov", image: MassaShukhrat, instagram: "https://instagram.com" },
+    { id: 15, name: "usoma_vosiyev", title: "usoma_vosiyev", image: UsomVosiyev, instagram: "https://instagram.com" }
   ];
 
   return (
